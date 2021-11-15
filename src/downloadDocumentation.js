ab1795050e07344bd9e9d740de92e96f08994753
Moamen El-Tahwagi <moamen.el-tahwagi@umlaut.com>
2021-11-15 20:15:05 +0100

    set export type

--- src/downloadDocumentation.js
+++ src/downloadDocumentation.js
@@ -4,31 +4,40 @@ module.exports = {
   downloadDocumentation: function () {
     const aws = this.serverless.providers.aws;
     const stackName = aws.naming.getStackName(aws.getStage());
-    return this._getRestApiId(stackName).then((restApiId) => {
-      return aws.request('APIGateway', 'getExport', {
-        stageName: aws.getStage(),
-        restApiId: restApiId,
-        exportType: 'swagger',
-        parameters: {
-          extensions: extensionType(this.options.extensions),
-        },
-        accepts: createAWSContentType(this.options.outputFileName),
+    return this._getRestApiId(stackName)
+      .then((restApiId) => {
+        return aws.request('APIGateway', 'getExport', {
+          stageName: aws.getStage(),
+          restApiId: restApiId,
+          exportType: this.options.exportType
+            ? this.options.exportType
+            : 'swagger',
+          parameters: {
+            extensions: extensionType(this.options.extensions),
+          },
+          accepts: createAWSContentType(this.options.outputFileName),
+        });
+      })
+      .then((response) => {
+        this.fs.writeFileSync(this.options.outputFileName, response.body);
       });
-    }).then((response) => {
-      this.fs.writeFileSync(this.options.outputFileName, response.body);
-    });
   },
 
   _getRestApiId: function (stackName) {
     const aws = this.serverless.providers.aws;
-    return aws.request('CloudFormation', 'describeStacks', {StackName: stackName},
-      aws.getStage(),
-      aws.getRegion()
-    ).then((result) => {
-      return result.Stacks[0].Outputs
-        .filter(output => output.OutputKey === 'AwsDocApiId')
-        .map(output => output.OutputValue)[0];
-    });
+    return aws
+      .request(
+        'CloudFormation',
+        'describeStacks',
+        { StackName: stackName },
+        aws.getStage(),
+        aws.getRegion()
+      )
+      .then((result) => {
+        return result.Stacks[0].Outputs.filter(
+          (output) => output.OutputKey === 'AwsDocApiId'
+        ).map((output) => output.OutputValue)[0];
+      });
   },
 };
 
@@ -50,7 +59,12 @@ function createAWSContentType(outputFileName) {
 }
 
 function extensionType(extensionArg) {
-  const possibleExtensions = ['integrations', 'apigateway', 'authorizers', 'postman'];
+  const possibleExtensions = [
+    'integrations',
+    'apigateway',
+    'authorizers',
+    'postman',
+  ];
 
   if (possibleExtensions.includes(extensionArg)) {
     return extensionArg;
@@ -58,4 +72,3 @@ function extensionType(extensionArg) {
     return 'integrations';
   }
 }
-
